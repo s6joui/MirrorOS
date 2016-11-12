c17cc79099098a6c09bb09371c89a5e7665c9a12
Joui <joui1994@gmail.com>
2016-11-12 20:18:49 +0100

    improved speech recognition speed

--- system/core/speech-recognizer/mic.py
+++ system/core/speech-recognizer/mic.py
@@ -12,7 +12,10 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-TAP_TIME_SPACING = 7 #at least 7 seonds between taps
+TALKING_MIN_AMP = 0.01;
+MAX_TALK_SKIP_BLOCKS = 4;
+
+TAP_TIME_SPACING = 2 #at least 2 seonds between talks
 TAP_THRESHOLD = 0.15 #higher means less sensitive
 FORMAT = pyaudio.paInt16 
 SHORT_NORMALIZE = (1.0/32768.0)
@@ -67,39 +70,52 @@ if input_device_index >= 0:
          input_device_index = input_device_index,                           
          frames_per_buffer = INPUT_FRAMES_PER_BLOCK)   
 
-	timeSinceLastTap = 0;
+	timeSinceLastTalk = 0
+	timeSinceLastTap = 0
+	shouldRecord = False
 	while True:
 		try:
 			block = stream.read(INPUT_FRAMES_PER_BLOCK)
 		except IOError, e:
 			sys.stderr.write("%s"%(e))
 			#print( "Error recording: %s\n"%(e) )
-
-		amplitude = get_rms(block)
-		#print("AMP:%f THR:%f"%(amplitude,TAP_THRESHOLD))
-		if amplitude > TAP_THRESHOLD: # if its to loud...
-			if time.time()-timeSinceLastTap >= TAP_TIME_SPACING:
-					print "TRIG"
-					shouldRecord = True
-					timeSinceLastTap = time.time();
-				
-			if timeSinceLastTap >= RECORD_SECONDS and shouldRecord:
-				#Save file
-				shouldRecord = False
-				frames = []
-				
-				for i in range(0, int(input_device_rate / INPUT_FRAMES_PER_BLOCK * RECORD_SECONDS)):
-					data = stream.read(INPUT_FRAMES_PER_BLOCK)
-					frames.append(data)
+		
+		if shouldRecord == False:
+			amplitude = get_rms(block)
+			#print("AMP:%f THR:%f"%(amplitude,TAP_THRESHOLD))
+			if amplitude > TAP_THRESHOLD:
+				if time.time()-timeSinceLastTalk >= TAP_TIME_SPACING:
+						print "TRIG"
+						shouldRecord = True
+						timeSinceLastTap = time.time();
+		
+		if shouldRecord and time.time()-timeSinceLastTap >= 0.35:
+			#Save file
+			shouldRecord = False
+			frames = []
+			
+			notTalkingBlocks = -16;
+			while notTalkingBlocks < MAX_TALK_SKIP_BLOCKS:
+				data = stream.read(INPUT_FRAMES_PER_BLOCK)
+				amp = get_rms(data)
+				if amp > TALKING_MIN_AMP:
+					notTalkingBlocks = 0
+				else:
+					notTalkingBlocks = notTalkingBlocks + 1;
 					
-				waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-				waveFile.setnchannels(input_device_channels)
-				waveFile.setsampwidth(pa.get_sample_size(FORMAT))
-				waveFile.setframerate(input_device_rate)
-				waveFile.writeframes(b''.join(frames))
-				waveFile.close()		
+				frames.append(data)
 				
-				subprocess.call("./wavToText.sh -k="+apiKey,shell=True)
+			timeSinceLastTalk = time.time();
+
+			waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+			waveFile.setnchannels(input_device_channels)
+			waveFile.setsampwidth(pa.get_sample_size(FORMAT))
+			waveFile.setframerate(input_device_rate)
+			waveFile.writeframes(b''.join(frames))
+			waveFile.close()		
+			
+			subprocess.call("./wavToText.sh -k="+apiKey,shell=True)
+			
 else:
 	#Start mic selector
 	print "No microphone found. Please reconnect it and wait"
