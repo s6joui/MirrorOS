fdad98b07070b9c433ed24602579a3bcd9471e47
Joui <joui1994@gmail.com>
2016-11-14 14:27:37 +0100

    Possible fix for microphone crash

--- system/core/speech-recognizer/mic.py
+++ system/core/speech-recognizer/mic.py
@@ -12,15 +12,14 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-TALKING_MIN_AMP = 0.01;
-MAX_TALK_SKIP_BLOCKS = 4;
+TALKING_MIN_AMP = 0.009;
+MAX_TALK_SKIP_BLOCKS = 6;
 
-TAP_TIME_SPACING = 2 #at least 2 seonds between talks
+TAP_TIME_SPACING = 2 #at least 2 seconds between talks
 TAP_THRESHOLD = 0.15 #higher means less sensitive
 FORMAT = pyaudio.paInt16 
 SHORT_NORMALIZE = (1.0/32768.0)
-INPUT_BLOCK_TIME = 0.05
-RECORD_SECONDS = 4
+INPUT_BLOCK_TIME = 0.1375
 WAVE_OUTPUT_FILENAME = "file.wav"
 
 def get_rms(block):
@@ -61,7 +60,7 @@ for i in range(pa.get_device_count()):
 	
 if input_device_index >= 0:
 	#Start listening
-	INPUT_FRAMES_PER_BLOCK = int(input_device_rate*INPUT_BLOCK_TIME)
+	INPUT_FRAMES_PER_BLOCK = 1024#int(input_device_rate*INPUT_BLOCK_TIME)
 	                                                  
 	stream = pa.open(format = FORMAT,                      
          channels = input_device_channels,                          
@@ -72,7 +71,10 @@ if input_device_index >= 0:
 
 	timeSinceLastTalk = 0
 	timeSinceLastTap = 0
+	notTalkingBlocks = -16;
 	shouldRecord = False
+	shouldSave = False
+	saveFrames = []
 	while True:
 		try:
 			block = stream.read(INPUT_FRAMES_PER_BLOCK)
@@ -80,8 +82,8 @@ if input_device_index >= 0:
 			sys.stderr.write("%s"%(e))
 			#print( "Error recording: %s\n"%(e) )
 		
-		if shouldRecord == False:
-			amplitude = get_rms(block)
+		amplitude = get_rms(block)
+		if shouldRecord == False and shouldSave == False:
 			#print("AMP:%f THR:%f"%(amplitude,TAP_THRESHOLD))
 			if amplitude > TAP_THRESHOLD:
 				if time.time()-timeSinceLastTalk >= TAP_TIME_SPACING:
@@ -90,31 +92,33 @@ if input_device_index >= 0:
 						timeSinceLastTap = time.time();
 		
 		if shouldRecord and time.time()-timeSinceLastTap >= 0.35:
-			#Save file
-			shouldRecord = False
-			frames = []
-			
-			notTalkingBlocks = -16;
-			while notTalkingBlocks < MAX_TALK_SKIP_BLOCKS:
-				data = stream.read(INPUT_FRAMES_PER_BLOCK)
-				amp = get_rms(data)
-				if amp > TALKING_MIN_AMP:
+			#Record file			
+			if notTalkingBlocks < MAX_TALK_SKIP_BLOCKS:
+				if amplitude > TALKING_MIN_AMP:
 					notTalkingBlocks = 0
 				else:
 					notTalkingBlocks = notTalkingBlocks + 1;
-					
-				frames.append(data)
+				saveFrames.append(block)
+			else:
+				shouldRecord = False
+				shouldSave = True
 				
+		if shouldSave:
+			#Save file
+			shouldSave = False
 			timeSinceLastTalk = time.time();
 
 			waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 			waveFile.setnchannels(input_device_channels)
 			waveFile.setsampwidth(pa.get_sample_size(FORMAT))
 			waveFile.setframerate(input_device_rate)
-			waveFile.writeframes(b''.join(frames))
+			waveFile.writeframes(b''.join(saveFrames))
 			waveFile.close()		
 			
 			subprocess.call("./wavToText.sh -k="+apiKey,shell=True)
+			saveFrames = []
+			notTalkingBlocks = -16;
+
 			
 else:
 	#Start mic selector
