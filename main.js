c1fe68bf9efced2bd13fb2c017b8f95aa7b55402
joui <joui1994@gmail.com>
2017-03-14 12:43:20 +0100

    set to fullscreen

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1080, height: 1920,frame: true})
+  win = new BrowserWindow({width: 1080, height: 1920,frame: false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +18,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
